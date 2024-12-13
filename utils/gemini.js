926058ec83f7fa8cb9da212e980ca4f74ca069f3
Joshua <akinleyejoshua.dev@gmail.com>
2024-12-13 17:59:19 +0100

    Update gemini.js

--- utils/gemini.js
+++ utils/gemini.js
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 
 export async function prompt(prompt) {
     // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp"});
   
     const chat = model.startChat();
   
